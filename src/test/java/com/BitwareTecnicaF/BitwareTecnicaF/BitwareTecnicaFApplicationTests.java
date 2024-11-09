4daec10665b57717245715b8c531d6a31183696d
josejuniorpg <yunipgx@protonmail.com>
2024-11-09 16:54:53 -0600

    Base

---
+++ src/test/java/com/BitwareTecnicaF/BitwareTecnicaF/BitwareTecnicaFApplicationTests.java
@@ -0,0 +1,13 @@
+package com.BitwareTecnicaF.BitwareTecnicaF;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BitwareTecnicaFApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
